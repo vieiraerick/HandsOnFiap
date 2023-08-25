903f68c82fce3ee7f5bf490b41eba0a1d2fd5073
Erick Vieira <magaiverstz@gmail.com>
2023-08-24 21:37:21 -0300

    Mudança de logica

---
+++ Aula Inaugural/A2/HandsOnAulaInaugural/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.Dynamic;
+using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    int statusCode = 0;
+
+    int valorFinal = 750;
+
+    do
+    {
+        var password1 = valorFinal.ToString() + "a";
+
+        dynamic body = new ExpandoObject();
+
+        body.Key = password1;
+
+        var content = new StringContent(JsonConvert.SerializeObject(body), null, "application/json"); ;
+
+        var json = await client.PostAsync(
+            "https://fiap-inaugural.azurewebsites.net/fiap",
+            content
+            );
+
+        Console.WriteLine(password1);
+        Console.WriteLine(json);
+
+        valorFinal--;
+    } while (statusCode != 200);
+
+}
+
